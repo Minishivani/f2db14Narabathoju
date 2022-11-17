18aa87145c825c1fbbed4bd221da57cc8d4ebdf3
Minishivani <111610387+Minishivani@users.noreply.github.com>
2022-11-16 18:44:15 -0600

    added detail and create for single resource

--- controllers/test.js
+++ controllers/test.js
@@ -109,3 +109,46 @@ exports.drinks_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);      
     }
    };
+
+   // Handle a show one view with id specified by query
+exports.drinks_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await drinks.findById( req.query.id)
+    res.render('drinksdetail',
+   { title: 'Drinks Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for creating a drinks.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.drinks_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('drinkscreate', { title: 'Drinks Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.drinks_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await drinks.findById(req.query.id)
+    res.render('drinksupdate', { title: 'Drinks Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   
--- routes/drinks.js
+++ routes/drinks.js
@@ -4,4 +4,16 @@ var router = express.Router();
 
 router.get('/', drinks_controlers.drinks_view_all_Page );
 router.get('/drinks/:id', drinks_controlers.drinks_detail); 
+
+router.get('/detail', drinks_controlers.drinks_view_one_Page);
+
+/* GET create drinks page */
+router.get('/create', drinks_controlers.drinks_create_Page);
+
+/* GET create update page */
+router.get('/update', drinks_controlers.drinks_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', drinks_controlers.drinks_delete_Page);
+
 module.exports = router;
